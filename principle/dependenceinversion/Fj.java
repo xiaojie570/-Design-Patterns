d6f583be1314495ddb347dd5d41ebc727522acd1
fuxiaojie <457732323@qq.com>
2019-01-05 13:44:34 +0800

    开闭原则 依赖倒置原则

---
+++ principle/dependenceinversion/Fj.java
@@ -0,0 +1,13 @@
+package dependenceinversion;
+
+/**
+ * Created by lenovo on 2019/1/5.
+ */
+public class Fj {
+    public void studyJavaCourse() {
+        System.out.println("fj在学习java课程");
+    }
+    public void studyFECourse() {
+        System.out.println("fj在学习FE课程");
+    }
+}
---
+++ principle/dependenceinversion/Main.java
@@ -0,0 +1,12 @@
+package dependenceinversion;
+
+/**
+ * Created by lenovo on 2019/1/5.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Fj fj = new Fj();
+        fj.studyJavaCourse();
+        fj.studyFECourse();
+    }
+}
